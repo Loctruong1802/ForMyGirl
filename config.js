b909b5f4d901521ec10e55fb7c60af1322a447b2
Loctruong1802 <loctruong1802@gmail.com>
2024-05-15 21:06:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "Dear Qynh",
+    introTitle: 'Gửi miu',
+    introDesc: 'Ùmmm thì hoq có gi hết chỉ là anh rất muốn ở bên qynh nên là em đừng rời xa anh nhá',
+    btnIntro: 'Oey',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
